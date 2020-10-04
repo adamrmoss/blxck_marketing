3eaddfabf067937e79a53d51739103c932f3a3cd
brennaMacQuarrie <brennamacqurrie@gmail.com>
2020-10-03 22:29:32 -0500

    gsap

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 const bMac = {};
 
-// bMac.animationElement = $('.animationElement');
-// let $window = $(window);
-
-// bMac._window.on('scroll resize', bMac.ifVisible);
-// bMac._window.trigger('scroll');
-
-// bMac.ifVisible = function() {
-//     let windowHeight = bMac._window.height();
-//     let windowTop = bMac._window.scrollTop();
-//     let windowBottom = (windowTop + windowHeight);
-
-//     $.each(bMac._animationElement, function() {
-//         let $element = $(this);
-//         let elementHeight = $element.outerHeight();
-//         let elementTop = $element.offset().top;
-//         let elementBottom = (elementTop + elementHeight);
-
-//         if ((elementBottom >= windowTop) && (elementTop <= windowBottom)) {
-//             $element.addClass('inView');
-//         } else {
-//             $element.removeClass('inView');
-//         }
-//     });
-// }
 
 
 // function to make my burger menu appear
