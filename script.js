592537a01e8bfa94e8cae7d5be7b02a46d368d67
brennaMacQuarrie <brennamacqurrie@gmail.com>
2020-10-04 21:56:36 -0500

    sweet mother of gsap

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// AOS.init();
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.shiftLeft', {
+    scrollTrigger: {
+        trigger: '.shiftLeft',
+        toggleActions: 'restart pause resume reset',
+        scrub: true,
+    },
+    duration: 7, x: 900});
+
+// animates between states
+gsap.from('.proj', {
+    scrollTrigger: {
+        trigger: '.proj',
+        toggleActions: 'restart pause resume reset',
+        scrub: true,
+    },
+    duration: 1, opacity: 0, y: 190});
+
+gsap.from('.whiteLine', {
+    scrollTrigger: {
+        trigger: '.whiteLine',
+        toggleActions: 'restart pause resume reset',
+        scrub: true,
+    },
+    duration: 8, width: "0%", stagger: 1.5});
+
 const bMac = {};
 
 
@@ -12,7 +40,6 @@ bMac.hamburger = function () {
 
 bMac.init = function() {
     bMac.hamburger();
-    // new WOW().init();
 }
 
 $(document).ready(function () {
