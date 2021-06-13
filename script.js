1e162aeb98f2ef25e69af3e956f1a60108e23668
brennaMacQuarrie <brennamacqurrie@gmail.com>
2021-06-12 19:15:43 -0600

    cleaned up

--- script.js
+++ script.js
@@ -9,24 +9,13 @@ gsap.from('.shiftLeft', {
     },
     duration: 7, x: 900
 });
-
-// PROJECT JUMP
-gsap.from('.peach, .justLucy', {
+gsap.from('.shiftRight', {
     scrollTrigger: {
-        trigger: '.peach',
+        trigger: '.shiftRight',
         toggleActions: 'restart pause resume reset',
-        scrub: true,
+        scrub: 2,
     },
-    duration: 1, x: -90, stagger: 4
-});
-    
-gsap.from('.quickExg', {
-    scrollTrigger: {
-        trigger: '.quickExg',
-        toggleActions: 'restart pause resume reset',
-        scrub: true,
-    },
-    duration: 1, x: 90
+    duration: 7, x: -900
 });
 
 // ABOUT ME TEXT ROLL 
@@ -85,20 +74,20 @@ gsap.from('.skillDiv i', {
     duration: 12, fontSize: '0.2rem', stagger: 5.6
 });
 
-gsap.to('.arrowVisuals .top, .arrowVisuals .bottom', {
-    scrollTrigger: {
-        trigger: '.arrowVisuals',
-        toggleActions: 'restart pause resume reset',
-        scrub: true,
-    },
-    duration: 5, left: '100%'
-})
+// gsap.to('.arrowVisuals .top, .arrowVisuals .bottom', {
+//     scrollTrigger: {
+//         trigger: '.arrowVisuals',
+//         toggleActions: 'restart pause resume reset',
+//         scrub: true,
+//     },
+//     duration: 5, left: '100%'
+// })
 
 
 const bMac = {};
 
-bMac.contact = $('.contactClick');
-bMac.arrowContact = $('.arrowContact');
+// bMac.contact = $('.contactClick');
+// bMac.arrowContact = $('.arrowContact');
 bMac.cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
@@ -114,74 +103,74 @@ document.addEventListener('click', () => {
 
 
 // TOGGLE HEADER IMAGE FUNCTION 
-bMac.loadHeader = 2;
+// bMac.loadHeader = 2;
 
-document.querySelector('.arrow').onclick = () => {
+// document.querySelector('.arrow').onclick = () => {
   // if we're about to move to image1
   // this timeout allows img3 to drop below 1>> 
   // first drop z of 3, then give 1 width:100 and higher z, then give 3 width:0
-    if (bMac.loadHeader === 1) {
-        $('#headerImg3').css('z-index', '-10');
-        setTimeout(() => {
-            $('#headerImg1')
-                .css('width', '100%')
-                .css('z-index', '-9');
-        }, 100);
-        setTimeout(() => {
-            $('#headerImg3').css('width', '0');
-        }, 800);
+    // if (bMac.loadHeader === 1) {
+    //     $('#headerImg3').css('z-index', '-10');
+    //     setTimeout(() => {
+    //         $('#headerImg1')
+    //             .css('width', '100%')
+    //             .css('z-index', '-9');
+    //     }, 100);
+    //     setTimeout(() => {
+    //         $('#headerImg3').css('width', '0');
+    //     }, 800);
         // if we're not moving from 1 to 3, give width:100 & z:-9 to all
-    } else {
-        $('#headerImg'+bMac.loadHeader)
-            .css('width', '100%')
-            .css('z-index', '-9');
-    }
+    // } else {
+    //     $('#headerImg'+bMac.loadHeader)
+    //         .css('width', '100%')
+    //         .css('z-index', '-9');
+    // }
     
-    let previousImg = bMac.loadHeader - 1;
+//     let previousImg = bMac.loadHeader - 1;
 
-    if (bMac.loadHeader === 3) {
-        // otherwise would go back to 2
-        bMac.loadHeader = 1;
-    } else if (bMac.loadHeader === 1) {
-        // because 1 - 1 = 0, and we want to do stuff to 3 in this scenario
-        previousImg = 3;
-        bMac.loadHeader++;
-    } else {
-        bMac.loadHeader++;
-    }
+//     if (bMac.loadHeader === 3) {
+//         // otherwise would go back to 2
+//         bMac.loadHeader = 1;
+//     } else if (bMac.loadHeader === 1) {
+//         // because 1 - 1 = 0, and we want to do stuff to 3 in this scenario
+//         previousImg = 3;
+//         bMac.loadHeader++;
+//     } else {
+//         bMac.loadHeader++;
+//     }
     
-    // this part i dont quite understand
-    if (bMac.loadHeader !== 2) {
-        setTimeout(() => {
-            $('#headerImg' + previousImg)
-                .css('width', '0')
-                .css('z-index', '-10');
-        }, 800);
-    }
-}
+//     // this part i dont quite understand
+//     if (bMac.loadHeader !== 2) {
+//         setTimeout(() => {
+//             $('#headerImg' + previousImg)
+//                 .css('width', '0')
+//                 .css('z-index', '-10');
+//         }, 800);
+//     }
+// }
 
 
-bMac.contactModal = function() {
-    bMac.contact.click(function () {
-        $('.modal').toggleClass(`activeView`);
-    })
-    bMac.arrowContact.click(function() {
-        $('.modal').toggleClass(`activeView`);
-    })
-}
-// function to make my burger menu appear
-bMac.hamburger = function () {
-    $(`.hamburg`).click(function () {
-        $(this).toggleClass(`active`);
-        $(`.dropdownMenu`).toggleClass(`active`);
-    });
-}
+// bMac.contactModal = function() {
+//     bMac.contact.click(function () {
+//         $('.modal').toggleClass(`activeView`);
+//     })
+//     bMac.arrowContact.click(function() {
+//         $('.modal').toggleClass(`activeView`);
+//     })
+// }
+// // function to make my burger menu appear
+// bMac.hamburger = function () {
+//     $(`.hamburg`).click(function () {
+//         $(this).toggleClass(`active`);
+//         $(`.dropdownMenu`).toggleClass(`active`);
+//     });
+// }
 
-bMac.init = function() {
-    bMac.hamburger();
-    bMac.contactModal();
-}
+// bMac.init = function() {
+//     bMac.hamburger();
+//     bMac.contactModal();
+// }
 
-$(document).ready(function () {
-    bMac.init();
-})
+// $(document).ready(function () {
+//     bMac.init();
+// })
