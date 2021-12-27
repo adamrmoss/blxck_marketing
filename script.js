51559778087f623ede9441dded3cc78091541f52
brennaMacQuarrie <brennamacquarrie@gmail.com>
2021-12-26 17:36:14 -0700

    removed init

--- script.js
+++ script.js
@@ -76,16 +76,11 @@ bMac.cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
     bMac.cursor.setAttribute('style', 'top: '+(e.pageY - 20)+"px; left: "+(e.pageX - 20)+'px;');
-})
+});
 
 document.addEventListener('click', () => {
     bMac.cursor.classList.add('expand');
     setTimeout(() => {
         bMac.cursor.classList.remove('expand');
     }, 500)
-})
-
-
-$(document).ready(function () {
-    bMac.init();
-})
+});
