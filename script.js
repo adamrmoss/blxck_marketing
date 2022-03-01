06357b301e22957b8a22c0fca61bc80647ec9816
brennaMacQuarrie <brennamacquarrie@gmail.com>
2022-03-01 14:32:42 -0500

    testing mobile

--- script.js
+++ script.js
@@ -18,42 +18,42 @@ gsap.to('.urbrand', {
     duration: 3, x: 800
 });
 
-gsap.to('.shiftRight1', {
-    scrollTrigger: {
-        trigger: '.what',
-        toggleActions,
-        scrub: 2,
-    },
-    duration: 10, x: 600
-});
 gsap.to('.shiftLeft1', {
     scrollTrigger: {
-        trigger: '.about',
+        trigger: '#about-us',
         toggleActions,
         scrub: 2,
     },
-    duration: 10, x: -600
+    duration: 10, x: -500
 });
-gsap.to('.shiftRight2', {
+gsap.to('.shiftRight1', {
     scrollTrigger: {
-        trigger: '.contact',
+        trigger: '#what-we-do',
         toggleActions,
         scrub: 2,
     },
-    duration: 10, x: 600
+    duration: 10, x: 500
 });
 gsap.to('.shiftLeft2', {
     scrollTrigger: {
-        trigger: '.why',
+        trigger: '#our-services',
         toggleActions,
         scrub: 2,
     },
-    duration: 10, x: -600
+    duration: 10, x: -650
+});
+gsap.to('.shiftRight2', {
+    scrollTrigger: {
+        trigger: '#contact',
+        toggleActions,
+        scrub: 2,
+    },
+    duration: 10, x: 500
 });
 
 gsap.to('.fade', {
     scrollTrigger: {
-        trigger: '.about',
+        trigger: '#about-us',
         toggleActions,
         scrub: 2,
     },
@@ -120,32 +120,3 @@ document.addEventListener('click', () => {
     }, 500)
 });
 
-// const cursor = document.getElementById("cursor");
-// // const cursorBallBig = document.querySelector(".circle-big");
-// const planet = document.querySelector(".cursor-color");
-
-// let posS = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-// let posB = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-// let mouse = { x: posS.x, y: posS.y };
-// const speed = 0.1;
-// let fpms = 60 / 1000;
-
-// window.addEventListener("mousemove", (e) => {
-//   mouse.x = e.x;
-//   mouse.y = e.y;
-// });
-
-// const xSetPlanet = gsap.quickSetter(planet, "x", "px");
-// const ySetPlanet = gsap.quickSetter(planet, "y", "px");
-
-// gsap.ticker.add((deltaTime) => {
-//   let delta = deltaTime * fpms;
-//   let dt = 1.0 - Math.pow(1.0 - speed, delta);
-
-//   posS.x += mouse.x - posS.x;
-//   posS.y += mouse.y - posS.y;
-//   posB.x += (mouse.x - posB.x) * dt;
-//   posB.y += (mouse.y - posB.y) * dt;
-//   xSetPlanet(posB.x);
-//   ySetPlanet(posB.y);
-// });
