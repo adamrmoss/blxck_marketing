08d1630ad93048dbe10a55a9c2c5fe8deb8ba9ff
brennaMacQuarrie <brennamacquarrie@gmail.com>
2022-02-28 20:58:27 -0500

    changes

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ gsap.to('.shiftRight2', {
         toggleActions,
         scrub: 2,
     },
-    duration: 10, x: 800
+    duration: 10, x: 600
 });
 gsap.to('.shiftLeft2', {
     scrollTrigger: {
@@ -79,7 +79,6 @@ gsap.from('.whiteLine2', {
     duration: 8, width: "0", stagger: 1
 });
 
-
 const nav = document.getElementById('nav')
 
 const showHideHeader = gsap.from(nav, {
@@ -96,7 +95,7 @@ ScrollTrigger.create({
     }
 })
 
-const cursor = document.querySelector('.cursor')
+
 function toggleText(el) {
     const text = el.nextElementSibling;
     text.classList.toggle('hidden');
@@ -108,7 +107,31 @@ function toggleText(el) {
       }
 }
 
+const cursor = document.getElementById("cursor");
+const planet = document.querySelector(".cursor-color");
 
+let posS = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+let posB = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+let mouse = { x: posS.x, y: posS.y };
+const speed = 0.1;
+let fpms = 60 / 1000;
 
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
 
+const xSetPlanet = gsap.quickSetter(planet, "x", "px");
+const ySetPlanet = gsap.quickSetter(planet, "y", "px");
 
+gsap.ticker.add((deltaTime) => {
+  let delta = deltaTime * fpms;
+  let dt = 1.0 - Math.pow(1.0 - speed, delta);
+
+  posS.x += mouse.x - posS.x;
+  posS.y += mouse.y - posS.y;
+  posB.x += (mouse.x - posB.x) * dt;
+  posB.y += (mouse.y - posB.y) * dt;
+  xSetPlanet(posB.x);
+  ySetPlanet(posB.y);
+});
